134beac88e026047ecd0f7e67355bb2f2035f452
Benicio <beniciohossi38@gmail.com>
2025-05-15 10:37:53 +0100

    Atualizando

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 const somar = (numb1,numb2) => {
     return numb1 - numb2;
+}
 
-=======
+
 const subtriarmultiplicar  = (num1,num2) => {
     return num1 * num2;
->>>>>>> bbfaf4b08a163d5f5c2534910e551ee48fdffaeb
 }
-=======
-
-const somar = (numb1, numb2) => {
-  return numb1 + numb2;
-}
-
-const subtriarmultiplicar  = (num1,num2) => {
-    return num1 * num2;}
->>>>>>> 9b510e7f3f0482617ce0eeb34124efc29aff410b
